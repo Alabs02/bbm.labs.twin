8ff0e239effeda2f7f25af897f82b33edb7649d3
Alabs02 <alabson.inc@gmail.com>
2024-06-23 14:20:55 +0100

    Changes

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,6 @@
 const cors = require("./cors.js");
 const limiter = require("./rateLimiter.js");
 const listRoutes = require("./listRoutes.js");
-const sessionMiddleware = require("./sessionMiddleware.js");
+const sessionMiddleware = require("./session.js");
 
 module.exports = { cors, listRoutes, limiter, sessionMiddleware };
